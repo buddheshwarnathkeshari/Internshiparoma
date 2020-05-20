89195671991e02b95339b30493bd8f0804e38b59
saitej-janjirala <janjiralasai420@gmail.com>
2020-05-20 16:49:08 +0530

    Merge branch 'master' of D:\Internshiparoma with conflicts.

--- app/src/main/java/com/buddheshwar/internshiparoma/Mainscreen.java
+++ app/src/main/java/com/buddheshwar/internshiparoma/Mainscreen.java
@@ -2,6 +2,7 @@ package com.buddheshwar.internshiparoma;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,16 +10,22 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.buddheshwar.internshiparoma.fragments.DashboardFragment;
+import com.buddheshwar.internshiparoma.fragments.Internships;
 import com.buddheshwar.internshiparoma.resume.EditResume;
 import com.buddheshwar.internshiparoma.resume.Resume;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class Mainscreen extends AppCompatActivity {
@@ -29,27 +36,19 @@ public class Mainscreen extends AppCompatActivity {
     ImageView menu;
     LinearLayout actionLayout;
     TextView appTitle;
+    Toolbar toolbar;
+    BottomNavigationView bottomnavigationview;
     public static boolean resumeFlag = false;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen);
-
+        bottomnavigationview=findViewById(R.id.bottomnav);
         mainscreenContainer=findViewById(R.id.container_mainscreen);
         navigationDrawerLayout=findViewById(R.id.navigation_drawer_view);
         drawerLayout=findViewById(R.id.drawerLayout);
-        actionLayout=findViewById(R.id.actionLayout);
-        menu=actionLayout.findViewById(R.id.menu_img);
-        appTitle=actionLayout.findViewById(R.id.app_title);
-        menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                drawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
+        toolbar=findViewById(R.id.toolbar);
+        settoolbar();
         navigationDrawerLayout.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -57,7 +56,8 @@ public class Mainscreen extends AppCompatActivity {
                 switch (menuItem.getItemId()) {
                     case R.id.home_drawer: {
                         fr=new DashboardFragment();
-                        openhome(fr);
+                        openfragment(fr);
+                        getSupportActionBar().setTitle("Dashboard");
                         return false;
                     }
                     case R.id.resume_drawer: {
@@ -73,8 +73,8 @@ public class Mainscreen extends AppCompatActivity {
                         Intent i=new Intent(Mainscreen.this, Resume.class);
                         startActivity(i);
                         drawerLayout.closeDrawer(GravityCompat.START);
-                      //  appTitle.setText("RESUME");
-
+                        getSupportActionBar().setTitle("Dashboard");
+                        //  appTitle.setText("RESUME");
                         return false;
 
                     }
@@ -85,7 +85,39 @@ public class Mainscreen extends AppCompatActivity {
                 }
             }
         });
-        openhome(new DashboardFragment());
+        bottomnavigationview.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home_bottom_nav:{
+                        Fragment fragment=getSupportFragmentManager().findFragmentById(R.id.container_mainscreen);
+                        if(!new DashboardFragment().equals(fragment)){
+                            item.setCheckable(true);
+                            item.setChecked(true);
+                            getSupportActionBar().setTitle("Dashboard");
+                            openfragment(new DashboardFragment());
+                            return false;
+                        }
+                    }
+                    case R.id.internship_bottom_nav:{
+                        item.setCheckable(true);
+                        item.setChecked(true);
+                        getSupportActionBar().setTitle("Internships");
+                        openfragment(new Internships());
+                        return false;
+
+                    }
+                    case R.id.chat_bottom_nav:{
+                        return false;
+                    }
+                    default:{
+                        return false;
+                    }
+                }
+            }
+        });
+        openfragment(new DashboardFragment());
+        getSupportActionBar().setTitle("Dashboard");
         View view=navigationDrawerLayout.getHeaderView(0);
         ImageView closedrawer=view.findViewById(R.id.closedrawer);
         closedrawer.setOnClickListener(new View.OnClickListener() {
@@ -95,17 +127,33 @@ public class Mainscreen extends AppCompatActivity {
             }
         });
     }
-    public void openhome(Fragment fr){
-        if(!resumeFlag){
-            getSupportFragmentManager().beginTransaction().replace(R.id.container_mainscreen,fr).addToBackStack(null).commit();
-            resumeFlag=true;
-        }
-        else{
-            getSupportFragmentManager().beginTransaction().replace(R.id.container_mainscreen,fr).commit();
-        }
-        drawerLayout.closeDrawer(GravityCompat.START);
-        appTitle.setText("Dashboard");
+    public void settoolbar(){
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_drawer,R.string.close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(false);
+        toggle.setHomeAsUpIndicator(R.drawable.ic_bar_chart);
+        toggle.setToolbarNavigationClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
 
+            }
+        });
+        toggle.syncState();
+    }
+    public void openfragment(Fragment fr){
+        getSupportFragmentManager().beginTransaction().replace(R.id.container_mainscreen,fr).commit();
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        drawerLayout.openDrawer(navigationDrawerLayout,true);
+        return true;
     }
 
     @Override
@@ -113,7 +161,14 @@ public class Mainscreen extends AppCompatActivity {
         if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
             this.drawerLayout.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            Fragment fragment=getSupportFragmentManager().findFragmentById(R.id.container_mainscreen);
+            assert fragment != null;
+            if (new DashboardFragment().equals(fragment)) {
+                ActivityCompat.finishAffinity(this);
+            }
+            else{
+                super.onBackPressed();
+            }
         }
         resumeFlag=false;
     }
---
+++ app/src/main/java/com/buddheshwar/internshiparoma/fragments/Internships.java
@@ -0,0 +1,50 @@
+package com.buddheshwar.internshiparoma.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.buddheshwar.internshiparoma.R;
+import com.buddheshwar.internshiparoma.adapters.InternshipsAdapter;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Internships extends Fragment {
+    RecyclerView mrecyclerview;
+    InternshipsAdapter madapter;
+    LinearLayoutManager linearLayoutManager;
+    public Internships() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_internships, container, false);
+        setHasOptionsMenu(true);
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.searchandfiltermenu,menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
