ae17db0b86e38ccdad07a2e1ada81f76da69a031
Munazza Fatma <fatmamunazza@gmail.com>
2020-06-01 13:45:23 +0530

    completed google place api

--- app/src/main/java/com/buddheshwar/internshiparoma/AsIntern.java
+++ app/src/main/java/com/buddheshwar/internshiparoma/AsIntern.java
@@ -1,33 +1,44 @@
 package com.buddheshwar.internshiparoma;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.common.api.Status;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.net.PlacesClient;
-import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
-import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
 
-import java.util.Arrays;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class AsIntern extends AppCompatActivity {
-AutoCompleteTextView ac;
+
+    AutoCompleteTextView ac,acCity;
+    private RequestQueue requestQueue;
+    private String apikey="AIzaSyBvzY_qtuY_6t3aiCC9D3RjjorEQXgUe5Y";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_as_intern);
+
+        requestQueue = Volley.newRequestQueue(AsIntern.this);
+
         ac=findViewById(R.id.actv);
+        acCity=findViewById(R.id.ac_city);
         String[] list=new String[]{"Ms.","Mr.","Mrs."};
         ArrayAdapter adapter=new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_list_item_1,list);
         ac.setAdapter(adapter);
@@ -39,10 +50,62 @@ AutoCompleteTextView ac;
             }
         });
 
+        acCity.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
 
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String url="https://maps.googleapis.com/maps/api/place/autocomplete/json?input=" + acCity.getText().toString() + "&key=" + apikey;
+                if((i+i2)==3){
 
+                    JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(
+                            Request.Method.GET,
+                            url,
+                            null,
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
 
+                                    try {
+                                        String status=response.getString("status");
+                                        JSONArray prediction=response.getJSONArray("predictions");
+                                        if(status.equalsIgnoreCase("ok")) {
 
+                                            String[] list2=new String[prediction.length()];
+
+                                            for(int i=0;i<prediction.length();i++){
+                                                JSONObject jsonObject= prediction.getJSONObject(i);
+                                                String description=jsonObject.getString("description");
+
+                                                list2[i]=description;
+                                            }
+                                            ArrayAdapter adapter=new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_list_item_1,list2);
+                                            acCity.setAdapter(adapter);
+                                            acCity.setThreshold(3);
+                                        }
+                                        else{
+                                           Log.d("msg",status);
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                        }
+                    });
+                    requestQueue.add(jsonObjectRequest);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) { }
+        });
+
+/*
         EditText currentCity=((EditText)findViewById(R.id.places_autocomplete_search_input));
 
         ((View)findViewById(R.id.places_autocomplete_search_button)).setVisibility(View.GONE);
@@ -54,10 +117,9 @@ AutoCompleteTextView ac;
         currentCity.setHintTextColor(getResources().getColor(R.color.light));
         currentCity.setPadding(15,10,10,10);
         currentCity.setTextColor(getResources().getColor(R.color.dark));
-        currentCity.setTextSize(18);
+        currentCity.setTextSize(18);*/
 
-        String apikey=getString(R.string.GOOGLE_PLACE_API_KEY);
-        if(!Places.isInitialized()){
+      /*  if(!Places.isInitialized()){
             Places.initialize(getApplicationContext(),apikey);
 
         }
@@ -73,22 +135,13 @@ AutoCompleteTextView ac;
 
             }
 
-
             @Override
             public void onError(@NonNull Status status) {
 
                 Toast.makeText(getApplicationContext(),status.toString(),Toast.LENGTH_SHORT).show();
             }
-        });
-
-
-
-
-
-
-
+        });*/
     }
-
 
     public void goToAsIntern2(View view) {
 
